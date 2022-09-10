2d6a910cdcd87f2ab25933ede15618131869985e
cosine0720 <cosine0720@gmail.com>
2022-09-10 16:15:37 +0800

    feat: add todo seeder

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const todoSchema = new Schema({
+
+const restaurantSchema = new Schema({
   name: {
     type: String, // 資料型別是字串
     required: true // 這是個必填欄位
@@ -39,4 +40,4 @@ const todoSchema = new Schema({
   }
 })
 
-module.exports = mongoose.model('Restaurant', todoSchema)
+module.exports = mongoose.model('Restaurant', restaurantSchema)
